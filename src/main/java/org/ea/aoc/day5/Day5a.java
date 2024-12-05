7528452f20ac316a5a31c60c4e62fdbc4962e006
Daniel Persson <mailto.woden@gmail.com>
2024-12-05 23:06:15 +0100

    after day5

---
+++ src/main/java/org/ea/aoc/day5/Day5a.java
@@ -0,0 +1,58 @@
+package org.ea.aoc.day5;
+
+import org.ea.aoc.Util;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day5a {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Util.readInput(false, 5, false);
+
+        Map<String, List<String>> rules = new HashMap<>();
+        int result = 0;
+        for (String line : lines) {
+            if (line.isBlank()) continue;
+            if (line.contains("|")) {
+                String[] arr = line.split("\\|");
+                if (!rules.containsKey(arr[0])) {
+                    rules.put(arr[0], new ArrayList<>());
+                }
+                rules.get(arr[0]).add(arr[1]);
+            }
+            if (line.contains(",")) {
+                Map<String, Integer> lineToCheck = new HashMap<>();
+                String[] splitArr = line.split(",");
+
+                for (int i = 0; i < splitArr.length; i++) {
+                    lineToCheck.put(splitArr[i], i);
+                }
+
+                boolean correct = true;
+                for (Map.Entry<String, List<String>> entry : rules.entrySet()) {
+                    if (lineToCheck.containsKey(entry.getKey())) {
+                        int start = lineToCheck.get(entry.getKey());
+                        for (String s : entry.getValue()) {
+                            if (lineToCheck.containsKey(s) && start > lineToCheck.get(s)) {
+                                correct = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (correct) {
+
+                    result += Integer.parseInt(splitArr[splitArr.length / 2]);
+                }
+            }
+        }
+
+        // 6236 - high
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/org/ea/aoc/day5/Day5b.java
@@ -0,0 +1,47 @@
+package org.ea.aoc.day5;
+
+import org.ea.aoc.Util;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Day5b {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Util.readInput(false, 5, false);
+
+        Map<String, List<String>> rules = new HashMap<>();
+        int result = 0;
+        for (String line : lines) {
+            if (line.isBlank()) continue;
+            if (line.contains("|")) {
+                String[] arr = line.split("\\|");
+                if (!rules.containsKey(arr[0])) {
+                    rules.put(arr[0], new ArrayList<>());
+                }
+                rules.get(arr[0]).add(arr[1]);
+            }
+            if (line.contains(",")) {
+                Map<String, Integer> lineToCheck = new HashMap<>();
+                String[] splitArr = line.split(",");
+
+                List<String> before = Arrays.asList(splitArr);
+                List<String> after = new ArrayList<>();
+                after.addAll(Arrays.asList(splitArr));
+
+                after.sort((a, b) -> {
+                    if (!rules.containsKey(a)) return 0;
+                    List<String> right = rules.get(a);
+                    if (right.contains(b)) return -1;
+                    return 0;
+                });
+
+                if (!before.toString().equals(after.toString())) {
+                    result += Integer.parseInt(after.get(after.size() / 2));
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
